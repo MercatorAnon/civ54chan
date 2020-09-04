e3d825b7b561b5ff70616abfa142439622f66b4b
MercatorAnon <70697098+MercatorAnon@users.noreply.github.com>
2020-09-04 13:19:55 +0200

    Add files via upload

---
+++ SQL/techrename.sql
@@ -0,0 +1,78 @@
+UPDATE	Technologies
+SET Description = "Shitposting"
+WHERE Description = "TXT_KEY_TECH_AGRICULTURE_TITLE";
+UPDATE	Technologies
+SET PortraitIndex = 0
+WHERE Type = "TECH_AGRICULTURE";
+UPDATE	Technologies
+SET Quote = "[NEWLINE][TAB][TAB] 'Look mom! I posted it again!'[NEWLINE][TAB][TAB]  - A faggot[NEWLINE][TAB]"
+WHERE Quote = "TXT_KEY_TECH_AGRICULTURE_QUOTE";
+UPDATE	Technologies
+SET IconAtlas = "TECH_ATLAS_4"
+WHERE Type = "TECH_AGRICULTURE";
+
+
+UPDATE	Technologies
+SET Description = "Furries"
+WHERE Description = "TXT_KEY_TECH_ANIMAL_HUSBANDRY_TITLE";
+UPDATE	Technologies
+SET PortraitIndex = 6
+WHERE Type = "TECH_ANIMAL_HUSBANDRY";
+UPDATE	Technologies
+SET Quote = "[NEWLINE][TAB][TAB] 'Die barneyfag'[NEWLINE][TAB][TAB]  - Anon on /v/[NEWLINE][TAB]"
+WHERE Quote = "TXT_KEY_TECH_ANIMAL_HUSBANDRY_QUOTE";
+UPDATE	Technologies
+SET IconAtlas = "TECH_ATLAS_4"
+WHERE Type = "TECH_ANIMAL_HUSBANDRY";
+
+UPDATE	Technologies
+SET Description = "Aimbots"
+WHERE Description = "TXT_KEY_TECH_ARCHERY_TITLE";
+UPDATE	Technologies
+SET PortraitIndex = 1
+WHERE Type = "TECH_ARCHERY";
+UPDATE	Technologies
+SET Quote = "[NEWLINE][TAB][TAB] 'IAMTHELAWIAMTHELAWIAMTHELAW'[NEWLINE][TAB][TAB]  - Judge Dredd raping a kid[NEWLINE][TAB]"
+WHERE Quote = "TXT_KEY_TECH_ARCHERY_QUOTE";
+UPDATE	Technologies
+SET IconAtlas = "TECH_ATLAS_4"
+WHERE Type = "TECH_ARCHERY";
+
+UPDATE	Technologies
+SET Description = "Minecraft"
+WHERE Description = "TXT_KEY_TECH_MINING_TITLE";
+UPDATE	Technologies
+SET PortraitIndex = 2
+WHERE Type = "TECH_MINING";
+UPDATE	Technologies
+SET Quote = "[NEWLINE][TAB][TAB] 'Comfy Minecraft thread'[NEWLINE][TAB][TAB]  - Archived 0/0/1[NEWLINE][TAB]"
+WHERE Quote = "TXT_KEY_TECH_MINING_QUOTE";
+UPDATE	Technologies
+SET IconAtlas = "TECH_ATLAS_4"
+WHERE Type = "TECH_MINING";
+
+UPDATE	Technologies
+SET Description = "Phishing"
+WHERE Description = "TXT_KEY_TECH_SAILING_TITLE";
+UPDATE	Technologies
+SET PortraitIndex = 8
+WHERE Type = "TECH_SAILING";
+UPDATE	Technologies
+SET Quote = "[NEWLINE][TAB][TAB] 'I am Prince Nwagramnabne Malahaktae and i need help.'[NEWLINE][TAB][TAB]  - Ethiopian hacker[NEWLINE][TAB]"
+WHERE Quote = "TXT_KEY_TECH_SAILING_QUOTE";
+UPDATE	Technologies
+SET IconAtlas = "TECH_ATLAS_4"
+WHERE Type = "TECH_SAILING";
+
+UPDATE	Technologies
+SET Description = "MS Paint"
+WHERE Description = "TXT_KEY_TECH_WRITING_TITLE";
+UPDATE	Technologies
+SET PortraitIndex = 5
+WHERE Type = "TECH_WRITING";
+UPDATE	Technologies
+SET Quote = "[NEWLINE][TAB][TAB] 'And M00t said 'ROW ROW FIGHT THE POWER''[NEWLINE][TAB][TAB]  - /b/tard[NEWLINE][TAB]"
+WHERE Quote = "TXT_KEY_TECH_WRITING_QUOTE";
+UPDATE	Technologies
+SET IconAtlas = "TECH_ATLAS_4"
+WHERE Type = "TECH_WRITING";
